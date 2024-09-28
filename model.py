9a34fc5442049da69a988ddfaa2d8b26c4968792
gogo0418 <tkgo11@naver.com>
2024-09-28 12:37:07 +0900

    Update model.py

--- model.py
+++ model.py
@@ -20,7 +20,7 @@ labels = []
 
 files = ['사실.txt', '대체로 사실.txt', '절반의 사실.txt', '대체로 사실 아님.txt', '전혀 사실 아님.txt']
 for file_name in files:
-    with open(file_name, "r", encoding="utf-8") as file:
+    with open("data/"+file_name, "r", encoding="utf-8") as file:
         lines = file.readlines()
         for line in lines:
             text = line.strip()
@@ -35,14 +35,15 @@ X_train, X_test, y_train, y_test = train_test_split(df['text'], df['label'], tes
 
 # 모델 생성 및 훈련
 vectorizer = CountVectorizer(stop_words=[word.strip() for word in stopwords])  # Tokenize stop words
-model = make_pipeline(vectorizer, MultinomialNB())
-model.fit(X_train, y_train)
+print(vectorizer)
+# model = make_pipeline(vectorizer, MultinomialNB())
+# model.fit(X_train, y_train)
 
-# 테스트 데이터로 평가
-predicted = model.predict(X_test)
-print(classification_report(y_test, predicted, zero_division=0))  # Set zero_division to 0
+# # 테스트 데이터로 평가
+# predicted = model.predict(X_test)
+# print(classification_report(y_test, predicted, zero_division=0))  # Set zero_division to 0
 
-# 사용자 입력 텍스트 분류
-user_input = input("평가할 텍스트를 입력하세요: ")
-predicted_label = model.predict([user_input])[0]
-print(f"예측된 정도: {predicted_label}")
+# # 사용자 입력 텍스트 분류
+# user_input = input("평가할 텍스트를 입력하세요: ")
+# predicted_label = model.predict([user_input])[0]
+# print(f"예측된 정도: {predicted_label}")
